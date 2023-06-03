b555e7c3ab0f84cf60c805e3ecc42bc93f171b25
ggoulartm <ggoulartm@gmail.com>
2023-06-03 12:33:09 -0300

    Interface de usuário

--- GUI.py
+++ GUI.py
@@ -1,14 +1,14 @@
 from tkinter import *
-import datetime
+from datetime import datetime
 
 def Publisher():
     datahora=datetime.now().strftime('%d/%m/%Y %H:%M')
-    print("evento publicado"+str(lb_events.get(ACTIVE)+vValor.get()+datahora)
-//acionar kafka
+    print("evento publicado"+str(lb_events.get(ACTIVE)+vValor.get()+datahora))
+#acionar kafka
 
 app = Tk()
 app.title("Sistema de Telemetria Veicular")
-app.geometry("300x100")
+app.geometry("400x200")
 
 vehicleEvents = ["Velocidade do veiculo","RPM do motor","Temperatura do motor","Nivel de combustivel","Localizacao GPS","Status das luzes"]
 Label(app,text="Evento:",background="#dde", foreground="#009",anchor=W).place(x=10,y=10,width=100,height=20)
@@ -20,10 +20,12 @@ for element in vehicleEvents:
 
 lb_events.pack()
 
-Label(app,text="Valor",background="#dde", foreground="#009",anchor=W).place(x=10,y=60,width=100,height=20)
+Label(app,text="Valor",background="#dde", foreground="#009",anchor=W).place(x=10,y=100,width=100,height=20)
 vValor=Entry(app)
-vValor.place(x=10,y=80,width=200,hieght=20)
+vValor.place(x=10,y=120,width=200,height=20)
 
 publicar=Button(app,text="Publicar",command=Publisher())
+publicar.place(x=30,y=145,width=50,height=20)
+
 
 app.mainloop()
